314b047010e1f0e4a551739260320af828726965
YinshengZ <zys2701999@gmail.com>
2022-08-18 00:41:02 +0100

    reconstructured treatment details with its related info

--- app/Services/TreatmentService.php
+++ app/Services/TreatmentService.php
@@ -14,20 +14,30 @@ class TreatmentService{
     public static function processTreatmentDetails($patient_id){
         $inventory_ids =[];
         $inventory_detail;
+        $patient_treatments = [];
         
         //group_concat() to concatenate and group by of treatments
-        $treatments = TreatmentDetails::select('treatment_id',DB::raw('group_concat(inventory_id) as treatments'),'units','quantity')
+        $treatments = TreatmentDetails::select('treatment_id',DB::raw('group_concat(inventory_id) as treatments'),'units','quantity','patient_id','quantity')
         ->where('patient_id',$patient_id)
         ->groupBy('treatment_id')
         ->get();
-        foreach($treatments as $treatment){
+        foreach($treatments as $index =>$treatment){
+            echo $index;
+            
             $inventory_id = $treatment->inventory_id;
-            $inventory_detail = Inventory::findMany($treatment->treatments);
-            print_r($inventory_id);
+            $treatment_details_id = explode(',',$treatment->treatments);
+            $inventory_detail = Inventory::findMany($treatment_details_id);
+            $patient_treatments[$index]['treatment_id'] = $treatment->treatment_id;
+            $patient_treatments[$index]['quantity']=  $treatment->quantity;
+            $patient_treatments[$index]['patient_id'] = $treatment->patient_id;
+            $patient_treatments[$index]['treatment_details']=$inventory_detail;
+            $patient_treatments[$index]['inventory_id']=$treatment_details_id;
+            //print_r($treatment->treatments);
+            echo "\n";
         } 
-        //TODO: successfully retrived inventory details, need to reconstruct the treatment details array object for returning
+        //TODO: add more essential treatment details
     
-        return $inventory_detail;
+        return $patient_treatments;
     }
 
 }
